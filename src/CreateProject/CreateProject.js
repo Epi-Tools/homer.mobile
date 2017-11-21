7354b90b9140274ccdc979c771c1eb6b65591621
Donavan Aziaka <donavan.aziaka@epitech.eu>
2017-11-21 22:31:28 +0100

    now you can create project from the application

--- src/CreateProject/CreateProject.js
+++ src/CreateProject/CreateProject.js
@@ -1,30 +1,80 @@
 import React from 'react';
 import {StyleSheet, Text, View, TextInput, Button, ScrollView} from 'react-native';
 
-// get user info http://10.14.58.12:8080/api/users
-// get projects info http://10.14.58.12:8080/api/
+const GLOBAL = require("../Global");
 
 
 export default class CreateProject extends React.Component {
-    description: string;
-    epices: string;
-    title: string;
-    follow1: string;
-    followdesc1: string;
-    follow2: string;
-    followdesc2: string;
-    delivery: string;
-    deliverydesc: string;
-
-
-    // texteditor : placeholder can make the text in the texteditor
 
     constructor(props) {
         super(props);
+        this.state = {
+            userId: 1,
+            spices: 0,
+            name: "",
+            description: "",
+            followUp: "",
+            followUp1: "",
+            delivery: "",
+            dateFollowUp: "2017-10-10T14:00:00",
+            dateFollowUp1: "2017-10-10T14:00:00",
+            dateDelivery: "2017-10-10T14:00:00"
+        };
     }
 
-    
-    
+    componentWillMount() {
+        fetch(GLOBAL.SERVER_URL + "/api/users/current", {
+            method: "GET",
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson.id);
+                this.setState({
+                    userId: responseJson.id
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+
+    handleNumberInput = (text) => {
+        if (/^\d+$/.test(text)) {
+            this.setState({
+                spices: text
+            });
+        }
+    }
+
+    onCreateProject() {
+        console.log(this.state.name);
+        let project = {
+            userId: this.state.userId,
+            spices: this.state.spices,
+            name: this.state.name,
+            description: this.state.description,
+            followUp: this.state.followUp,
+            followUp1: this.state.followUp1,
+            delivery: this.state.delivery,
+            dateFollowUp: "2017-10-10T14:00:00",
+            dateFollowUp1: "2017-10-10T14:00:00",
+            dateDelivery: "2017-10-10T14:00:00",
+        };
+        console.log(JSON.stringify(project));
+        fetch(GLOBAL.SERVER_URL + "/api/projects", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        })
+            .then((response) => response.json())
+            .then((responseData) => { console.log(responseData); })
+            .catch((err) => { console.log(err); });
+    }
+
     render() {
         return (
             <ScrollView showsVerticalScrollIndicator={false}>
@@ -37,58 +87,45 @@ export default class CreateProject extends React.Component {
                 <View style={styles.separator} />
                 <Text style={styles.title}>Name</Text>
                 <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                               onChangeText={(text) => this.setState({text})}
+                               onChangeText={text => this.setState({name: text})}
                                value={this.title}/>
                 <View style={styles.separator} />
                 <Text style={styles.title}>Spices</Text>
                     <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                               onChangeText={(text) => this.setState({text})}
+                               keyboardType='numeric'
+                               onChangeText={this.handleNumberInput()}
                                value={this.epices}/>
                 <View style={styles.separator} />
                 <Text style={styles.title}>Description</Text>
                 <TextInput style={{height: 80, borderColor: 'gray', borderWidth: 1}}
-                               onChangeText={(text) => this.setState({text})}
+                               onChangeText={(text) => this.setState({description: text})}
                                value={this.description} numberOfLines={4} multilines={true}/>
                 <View style={styles.separator} />
-                <Text style={styles.title}>Date FollowUp 1</Text>
-                <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                               onChangeText={(text) => this.setState({text})}
-                               value={this.desc} />
-                <View style={styles.separator} />
+
                 <Text style={styles.title}>FollowUp 1</Text>
                 <TextInput style={{height: 80, borderColor: 'gray', borderWidth: 1}}
-                               onChangeText={(text) => this.setState({text})}
+                               onChangeText={(text) => this.setState({followUp: text})}
                                value={this.follow1} numberOfLines={4} multilines={true}/>
-                <View style={styles.separator} />
-                <Text style={styles.title}>Date FollowUp 2</Text>
-                <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                               onChangeText={(text) => this.setState({text})}
-                               value={this.followdesc1}/>
+
                 <View style={styles.separator} />
                 <Text style={styles.title}>FollowUp 2</Text>
                 <TextInput style={{height: 80, borderColor: 'gray', borderWidth: 1}}
-                               onChangeText={(text) => this.setState({text})}
+                               onChangeText={(text) => this.setState({followUp1: text})}
                                value={this.follow2} numberOfLines={4} multilines={true}/>
-                <View style={styles.separator} />
-                <Text style={styles.title}>Date Delivery</Text>
-                <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                               onChangeText={(text) => this.setState({text})}
-                               value={this.followdesc2} numberOfLines={4} multilines={true}/>
+
                 <View style={styles.separator} />
                 <Text style={styles.title}>Delivery</Text>
                 <TextInput style={{height: 80, borderColor: 'gray', borderWidth: 1}}
-                               onChangeText={(text) => this.setState({text})}
+                               onChangeText={(text) => this.setState({delivery: text})}
                                value={this.delivery} numberOfLines={4} multilines={true}/>
                 <View style={styles.separator} />
-                <Button onPress={this.onPressButton} title="Create" color="#000000"/>
+                <Button onPress={() => this.onCreateProject()} title="Create" color="#000000"/>
                 </View>
             </ScrollView>
         );
     }
 
-    onPressButton() {
 
-    }
 }
 
 const styles = StyleSheet.create({
@@ -108,3 +145,23 @@ const styles = StyleSheet.create({
         height: 15
     },
 });
+
+/*
+    <Text style={styles.title}>Date FollowUp 1</Text>
+                <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                               onChangeText={(text) => this.setState({dateFollowUp: text})}
+                               value={this.desc} />
+                <View style={styles.separator} />
+
+                      <View style={styles.separator} />
+                <Text style={styles.title}>Date FollowUp 2</Text>
+                <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                               onChangeText={(text) => this.setState({dateFollowUp1: text})}
+                               value={this.followdesc1}/>
+
+                                        <View style={styles.separator} />
+                <Text style={styles.title}>Date Delivery</Text>
+                <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                               onChangeText={(text) => this.setState({dateDelivery: text})}
+                               value={this.followdesc2} numberOfLines={4} multilines={true}/>
+ */
